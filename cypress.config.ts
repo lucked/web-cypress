346aeb089cf02a7a6d6ad7a5bb973c748a35019f
Lucas rodrigues <lucasrodrigues@adopets.org>
2023-02-16 13:47:10 -0300

    more

---
+++ cypress.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'cypress'
+// Do not import plugins directly here. Do it in the file below
+import plugins from './cypress/plugins'
+
+const finalConfig = defineConfig({
+  projectId: 'ww5rer',
+  e2e: {
+    experimentalSessionAndOrigin: true,
+    defaultCommandTimeout: 30000,
+    pageLoadTimeout: 30000,
+    chromeWebSecurity: false,
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporterOptions.json',
+    },
+    screenshotsFolder: 'cypress/reports/mochareports/screenshots',
+    videosFolder: 'cypress/reports/mochareports/videos',
+    setupNodeEvents(on, config) {
+      // Setup plugins
+      config = plugins(on, config)
+
+      return config
+    },
+  },
+})
+
+export default finalConfig
